e5ae07ed643987c926c1e370e7457ff074faa38c
Justin Locsei <justin.locsei@gmail.com>
2026-08-22 11:41:30 -0400

    Expand utility types

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -2,3 +2,22 @@
  * Produce the most inclusive type for an array
  */
 export type AnyArray<T> = T[] | readonly T[];
+
+/**
+ * Accept a single item or a collection of items
+ */
+export type OneOrMany<T> = T | T[];
+
+/**
+ * Make a subset of fields in an object optional
+ */
+export type Optional<T extends object, K extends keyof T> =
+  & Omit<T, K>
+  & Partial<Pick<T, K>>;
+
+/**
+ * Require a subset of fields in an object
+ */
+export type Require<T extends object, K extends keyof T> =
+  & Omit<T, K>
+  & Required<Pick<T, K>>;
