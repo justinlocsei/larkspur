1a97e2f4cc99818fc230286f846f3345ccd0f073
Justin Locsei <justin.locsei@gmail.com>
2026-08-25 19:00:07 -0400

    Add a utility function to test thrown errors

--- src/tests.ts
+++ src/tests.ts
@@ -1,4 +1,28 @@
+import { assert } from 'vitest';
+
 import type { Flag } from './flags/types.js';
+
+/**
+ * Add a message to an assertion error
+ */
+function addAssertionMessage(assertion: string, message?: string): string {
+  return message ? `${message} (${assertion})` : assertion;
+}
+
+/**
+ * Coerce a value to an error
+ */
+function coerceError(value: unknown): Error {
+  if (value instanceof Error) {
+    return value;
+  } else if (typeof value === 'string') {
+    return new Error(value);
+  } else if (value === null || value === undefined) {
+    return new Error('Unknown error');
+  } else {
+    return new Error(JSON.stringify(value));
+  }
+}
 
 /**
  * Produce a readable representation of an object
@@ -23,6 +47,35 @@ export function checkConversion<I, O>(
       }`
     );
   }
+}
+
+/**
+ * Assert that a function throws an error matching the given check
+ */
+export function throwsWith(
+  check: () => void,
+  checkError: string | ((error: Error) => void),
+  message?: string
+): void {
+  try {
+    check();
+  } catch (reason) {
+    const error = coerceError(reason);
+
+    if (typeof checkError === 'string') {
+      assert.include(
+        error.toString(),
+        checkError,
+        addAssertionMessage('Unexpected error text', message)
+      );
+    } else {
+      checkError(error);
+    }
+
+    return;
+  }
+
+  assert.fail(addAssertionMessage('Expected an error to be thrown', message));
 }
 
 /**
