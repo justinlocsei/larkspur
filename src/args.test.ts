0ce3fbc66a3c021411f284a0b2cb4f3afd96fccd
Justin Locsei <justin.locsei@gmail.com>
2026-08-25 15:37:26 -0400

    Add code for normalizing arguments

---
+++ src/args.test.ts
@@ -0,0 +1,53 @@
+import { assert, describe, it } from 'vitest';
+
+import { NormalizedArgs } from './args.js';
+import { checkConversion } from './tests.js';
+
+describe('normalizeArgs', () => {
+  it('preserves simple args', () => {
+    checkConversion<string[], string[]>(
+      (i, o, m) =>
+        assert.sameOrderedMembers(new NormalizedArgs(i).normalized, o, m),
+      [
+        [[], []],
+        [['alfa'], ['alfa']],
+        [
+          ['--alfa', 'value'],
+          ['--alfa', 'value']
+        ],
+        [
+          ['--flag', 'needs quotes'],
+          ['--flag', 'needs quotes']
+        ]
+      ]
+    );
+  });
+
+  it('normalizes flags that use the equals sign', () => {
+    checkConversion<string[], string[]>(
+      (i, o, m) =>
+        assert.sameOrderedMembers(new NormalizedArgs(i).normalized, o, m),
+      [
+        [['--alfa=value'], ['--alfa', 'value']],
+        [
+          ['--alfa=1', '--bravo=2'],
+          ['--alfa', '1', '--bravo', '2']
+        ]
+      ]
+    );
+  });
+
+  it('handles quoted values in equals-sign bindings', () => {
+    checkConversion<string[], string[]>(
+      (i, o, m) =>
+        assert.sameOrderedMembers(new NormalizedArgs(i).normalized, o, m),
+      [
+        [['--alfa="value"'], ['--alfa', 'value']],
+        [
+          ['--alfa="one two"', '--bravo="three four"'],
+          ['--alfa', 'one two', '--bravo', 'three four']
+        ]
+      ]
+    );
+  });
+});
---
+++ src/args.ts
@@ -0,0 +1,47 @@
+import { flagToSetter, SETTER_PREFIX } from './flags/data.js';
+
+const QUOTED = /^['"]([^'"]*)['"]$/;
+const COMBINED_FLAG = new RegExp(`^${SETTER_PREFIX}(.+)=(.*)`);
+
+export class NormalizedArgs {
+  normalized: string[];
+
+  /**
+   * Create a wrapper that exposes a normalized version of CLI args
+   */
+  constructor(raw: string[]) {
+    this.normalized = this.normalize(raw);
+  }
+
+  /**
+   * Normalize CLI args
+   */
+  private normalize(args: string[]): string[] {
+    return args.reduce((previous: string[], arg) => {
+      const match = COMBINED_FLAG.exec(arg);
+
+      let name: string | undefined;
+      let rawValue: string | undefined;
+      let value: string | undefined;
+
+      if (match) {
+        name = match[1];
+        rawValue = match[2];
+
+        if (rawValue) {
+          const quoteMatch = QUOTED.exec(rawValue);
+          value = quoteMatch ? quoteMatch[1] : rawValue;
+        }
+      }
+
+      if (name && value) {
+        previous.push(flagToSetter(name));
+        previous.push(value);
+      } else {
+        previous.push(arg);
+      }
+
+      return previous;
+    }, []);
+  }
+}
