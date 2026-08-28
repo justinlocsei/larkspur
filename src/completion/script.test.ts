9f9ba9382cf9ebb6f05f2f9877b4bdfede3b3c1e
Justin Locsei <justin.locsei@gmail.com>
2026-08-28 16:54:07 -0400

    Add code to manage completion scripts

---
+++ src/completion/script.test.ts
@@ -0,0 +1,23 @@
+import { assert, describe, it } from 'vitest';
+
+import { formatScript } from './script.js';
+
+describe('formatScript', () => {
+  it('flattens lines', () => {
+    assert.deepEqual(formatScript(['alfa', 'bravo'], 2), 'alfa\nbravo');
+  });
+
+  it('applies indentation to nested groups', () => {
+    assert.deepEqual(
+      formatScript(['alfa', ['bravo', 'charlie'], 'delta'], 4),
+      'alfa\n    bravo\n    charlie\ndelta'
+    );
+  });
+
+  it('applies indentation to deeply nested line groups', () => {
+    assert.deepEqual(
+      formatScript(['alfa', ['bravo', ['charlie', 'delta']], 'echo'], 2),
+      'alfa\n  bravo\n    charlie\n    delta\necho'
+    );
+  });
+});
---
+++ src/completion/script.ts
@@ -0,0 +1,28 @@
+/**
+ * Lines in a generated completion script
+ */
+export type ScriptLines = Array<string | ScriptLines>;
+
+/**
+ * Format a script's lines as indented text
+ */
+export function formatScript(
+  lines: ScriptLines,
+  indent: number
+): string {
+  function format(ls: ScriptLines, level: number) {
+    const nesting = ' '.repeat(indent * level);
+
+    return ls.reduce((previous: string[], line) => {
+      if (typeof line === 'string') {
+        previous.push(`${nesting}${line}`);
+      } else {
+        previous.push(...format(line, level + 1));
+      }
+
+      return previous;
+    }, []);
+  }
+
+  return format(lines, 0).join('\n');
+}
