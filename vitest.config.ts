b704d4362436f3ffebcf01c40697133802cb8999
Justin Locsei <justin.locsei@gmail.com>
2026-08-29 07:42:01 -0400

    Always show full diffs

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    chaiConfig: { truncateThreshold: 0 },
     environment: 'node',
     include: ['src/**/*.test.ts']
   }
