d88f0313b93d253ad85bff1e1b7f777b5e09cdce
Justin Locsei <justin.locsei@gmail.com>
2026-08-29 15:57:02 -0400

    Add integration tests for flag parsing

---
+++ test/clis/flags.mjs
@@ -0,0 +1,51 @@
+import C, { run } from 'larkspur';
+
+const desc = 'A flag';
+
+await run({
+  defaults: C(
+    'Show flags with default values',
+    {
+      boolean: C.flag('boolean', desc, { default: true }),
+      number: C.flag('number', desc, { default: 1 }),
+      string: C.flag('string', desc, { default: 'alfa' })
+    },
+    async (flags) => {
+      console.log(JSON.stringify(flags));
+    }
+  ),
+
+  mixed: C(
+    'Show a mixture of optional and required flags',
+    {
+      number: C.flag('number', desc),
+      string: C.flag('string', desc, { required: true })
+    },
+    async (flags) => {
+      console.log(JSON.stringify(flags));
+    }
+  ),
+
+  optional: C(
+    'Show optional flags',
+    {
+      boolean: C.flag('boolean', desc),
+      number: C.flag('number', desc),
+      string: C.flag('string', desc)
+    },
+    async (flags) => {
+      console.log(JSON.stringify(flags));
+    }
+  ),
+
+  required: C(
+    'Show required flags',
+    {
+      number: C.flag('number', desc, { required: true }),
+      string: C.flag('string', desc, { required: true })
+    },
+    async (flags) => {
+      console.log(JSON.stringify(flags));
+    }
+  )
+});
---
+++ test/clis/flags.test.ts
@@ -0,0 +1,83 @@
+import { checkConversion } from '../../src/tests.js';
+import type { TestActions } from './helpers.js';
+import { assert, test } from './helpers.js';
+
+function checkFlags(
+  { checkOutput }: TestActions,
+  cases: Array<[string[], object]>
+) {
+  checkConversion<string[], object>(
+    (flags, values, message) =>
+      assert.deepEqual(JSON.parse(checkOutput(...flags)), values, message),
+    cases
+  );
+}
+
+test('flags', 'a CLI with flags', {
+  'allows optional flags to be omitted': (actions) => {
+    checkFlags(actions, [
+      [['optional'], { boolean: false }],
+      [['optional', '--boolean'], { boolean: true }],
+      [['optional', '--number', '1'], { boolean: false, number: 1 }],
+      [['optional', '--string', 'test'], { boolean: false, string: 'test' }],
+      [['optional', '--boolean', '--number', '1', '--string', 'test'], {
+        boolean: true,
+        number: 1,
+        string: 'test'
+      }]
+    ]);
+  },
+
+  'enforces the presence of required flags': ({ run }) => {
+    const result = run('required');
+
+    assert.equal(result.status, 1);
+    assert.include(result.stderr, 'required');
+  },
+
+  'supports required flags': (actions) => {
+    checkFlags(actions, [
+      [['required', '--number', '1', '--string', 'alfa'], {
+        number: 1,
+        string: 'alfa'
+      }],
+      [['required', '--number', '2', '--string', 'bravo'], {
+        number: 2,
+        string: 'bravo'
+      }]
+    ]);
+  },
+
+  'supports a mixture of optional and required flags': (actions) => {
+    checkFlags(actions, [
+      [['mixed', '--string', 'alfa'], {
+        string: 'alfa'
+      }],
+      [['mixed', '--number', '1', '--string', 'bravo'], {
+        number: 1,
+        string: 'bravo'
+      }]
+    ]);
+  },
+
+  'supports flags with default values': (actions) => {
+    checkFlags(actions, [
+      [['defaults'], { boolean: true, number: 1, string: 'alfa' }],
+      [['defaults', '--no-boolean'], {
+        boolean: false,
+        number: 1,
+        string: 'alfa'
+      }],
+      [['defaults', '--number', '2'], {
+        boolean: true,
+        number: 2,
+        string: 'alfa'
+      }],
+      [['defaults', '--string', 'bravo'], {
+        boolean: true,
+        number: 1,
+        string: 'bravo'
+      }]
+    ]);
+  }
+});
