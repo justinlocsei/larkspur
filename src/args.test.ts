caf4b41f6956ec0e99d7d79c6bc0e4aef93fa563
Justin Locsei <justin.locsei@gmail.com>
2026-08-30 13:22:21 -0400

    Reduce duplication

--- src/args.test.ts
+++ src/args.test.ts
@@ -3,48 +3,46 @@ import { assert, describe, it } from 'vitest';
 import { NormalizedArgs } from './args.js';
 import { checkConversion } from './tests.js';
 
-describe('normalizeArgs', () => {
-  it('preserves simple args', () => {
-    checkConversion<string[], string[]>(
+describe('NormalizeArgs', () => {
+  function checkArgs(tests: Array<[string[], string[]]>) {
+    checkConversion(
       (i, o, m) => assert.sameOrderedMembers(new NormalizedArgs(i).args, o, m),
-      [
-        [[], []],
-        [['alfa'], ['alfa']],
-        [
-          ['--alfa', 'value'],
-          ['--alfa', 'value']
-        ],
-        [
-          ['--flag', 'needs quotes'],
-          ['--flag', 'needs quotes']
-        ]
-      ]
+      tests
     );
+  }
+
+  it('preserves simple args', () => {
+    checkArgs([
+      [[], []],
+      [['alfa'], ['alfa']],
+      [
+        ['--alfa', 'value'],
+        ['--alfa', 'value']
+      ],
+      [
+        ['--flag', 'needs quotes'],
+        ['--flag', 'needs quotes']
+      ]
+    ]);
   });
 
   it('normalizes flags that use the equals sign', () => {
-    checkConversion<string[], string[]>(
-      (i, o, m) => assert.sameOrderedMembers(new NormalizedArgs(i).args, o, m),
+    checkArgs([
+      [['--alfa=value'], ['--alfa', 'value']],
       [
-        [['--alfa=value'], ['--alfa', 'value']],
-        [
-          ['--alfa=1', '--bravo=2'],
-          ['--alfa', '1', '--bravo', '2']
-        ]
+        ['--alfa=1', '--bravo=2'],
+        ['--alfa', '1', '--bravo', '2']
       ]
-    );
+    ]);
   });
 
   it('handles quoted values in equals-sign bindings', () => {
-    checkConversion<string[], string[]>(
-      (i, o, m) => assert.sameOrderedMembers(new NormalizedArgs(i).args, o, m),
+    checkArgs([
+      [['--alfa="value"'], ['--alfa', 'value']],
       [
-        [['--alfa="value"'], ['--alfa', 'value']],
-        [
-          ['--alfa="one two"', '--bravo="three four"'],
-          ['--alfa', 'one two', '--bravo', 'three four']
-        ]
+        ['--alfa="one two"', '--bravo="three four"'],
+        ['--alfa', 'one two', '--bravo', 'three four']
       ]
-    );
+    ]);
   });
 });
