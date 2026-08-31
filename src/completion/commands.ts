2f8011e185ba8f2dea59a52dc16f7b0b5dd9ef39
Justin Locsei <justin.locsei@gmail.com>
2026-08-31 09:08:47 -0400

    Add scaffolding for completion commands

---
+++ src/completion/commands.ts
@@ -0,0 +1,21 @@
+import type { CommandGroup } from '../commands/types.js';
+import C from '../factory.js';
+import { COMPLETION_SHELLS } from '../types.js';
+
+/**
+ * Define a command group to manage completions
+ */
+export function defineCompletionCommands(): CommandGroup {
+  return C.group('manage shell completions', {
+    generate: C(
+      'generate a completion script',
+      {
+        shell: C.flag('choice', 'a supported shell', {
+          choices: COMPLETION_SHELLS,
+          required: true
+        })
+      },
+      async () => {}
+    )
+  });
+}
