2d4effdb11f8e6bb7f30a5742dedd94dfa68f8b6
Justin Locsei <justin.locsei@gmail.com>
2026-08-31 08:44:10 -0400

    Use a test context

--- src/completion.test.ts
+++ src/completion.test.ts
@@ -2,12 +2,13 @@ import { assert, describe, it } from 'vitest';
 
 import { buildCompletions } from './completion.js';
 import C from './factory.js';
+import { createTestContext } from './tests.js';
 
 describe('buildCompletions', () => {
   it('supports bash', () => {
     const script = buildCompletions('bash', {
       commands: { noop: C('', async () => {}) },
-      name: 'test-cli-name'
+      context: createTestContext({ name: 'test-cli-name' })
     });
 
     assert.match(script, /^complete.*test-cli-name$/m);
