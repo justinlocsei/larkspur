c18f570bd687f0245a37c5d5c5952c370646fd44
Justin Locsei <justin.locsei@gmail.com>
2026-09-02 08:40:17 -0400

    Add an arbitrary for a command or flag description

--- src/tests/properties/definition.ts
+++ src/tests/properties/definition.ts
@@ -7,3 +7,5 @@ export const identifier = fc.stringMatching(IDENTIFIER_PATTERN);
 export const flagName = identifier.filter(name =>
   !name.startsWith(NEGATE_BOOLEAN)
 );
+
+export const description = fc.string({ unit: 'grapheme', minLength: 1 });
