4d7eeb44ec77ba4de2381fa4a218c48d952088a5
Justin Locsei <justin.locsei@gmail.com>
2026-09-02 15:28:40 -0400

    Expose an arbitrary

--- src/tests/properties.ts
+++ src/tests/properties.ts
@@ -1,7 +1,7 @@
 import { fc } from '@fast-check/vitest';
 
 export { entryPoint, singleCommand } from './properties/commands.js';
-export { flagName, identifier } from './properties/definition.js';
+export { description, flagName, identifier } from './properties/definition.js';
 export { flags } from './properties/flags.js';
 
 export const argv = fc.array(
