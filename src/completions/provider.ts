da45da9df9f1f56e955da547b13598655fced34f
Justin Locsei <justin.locsei@gmail.com>
2026-09-04 06:57:00 -0400

    Remove unused code

--- src/completions/provider.ts
+++ src/completions/provider.ts
@@ -65,13 +65,6 @@ export abstract class CompletionProvider {
   abstract provideScript(): CompletionScript;
 
   /**
-   * Format user-provided completion values for a shell
-   */
-  formatCompletions(completions: string[]): string {
-    return completions.length > 0 ? `${completions.join('\0')}\0` : '';
-  }
-
-  /**
    * Allow text to be safely used as an identifier
    */
   protected asIdentifier(text: string): string {
