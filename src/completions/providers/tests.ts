e82185d9a2f74f678af6b1f6d050ace26978c970
Justin Locsei <justin.locsei@gmail.com>
2026-09-05 19:19:53 -0400

    Consolidate code

--- src/completions/providers/tests.ts
+++ src/completions/providers/tests.ts
@@ -1,8 +1,41 @@
 import { assert, describe, it } from 'vitest';
 
-import { createCompletionCommands } from '../../../test/fixtures/commands.js';
+import type { CommandTree } from '../../commands/types.js';
+import C from '../../factory.js';
 import { createTestContext } from '../../tests.js';
 import type { CompletionProviderClass } from '../provider.js';
+
+/**
+ * Create commands that cover the essential parts of the completion system
+ */
+function createCompletionCommands(): CommandTree {
+  return {
+    bare: C('@bare-handler', async () => {}),
+    flags: C(
+      '@flag-handler',
+      { root: C.flag('string', '@root') },
+      async () => {}
+    ),
+    group: C.group('@group\n@newline', {
+      nested: C(
+        '@nested',
+        {
+          count: C.flag('number', '@count'),
+          custom: C.flag('string', '@custom', {
+            completion: async () => ['custom-alfa', 'custom-bravo']
+          }),
+          disabled: C.flag('boolean', '@disabled'),
+          enabled: C.flag('boolean', '@enabled', { default: true }),
+          mode: C.flag('choice', '@mode', {
+            choices: ['mode-alfa', 'mode-bravo']
+          }),
+          title: C.flag('string', "@title it's a: [value]")
+        },
+        async () => {}
+      )
+    })
+  };
+}
 
 const TEST_LABELS = {
   booleans: 'completes boolean flags in positive and negative form',
