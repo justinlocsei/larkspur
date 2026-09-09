7e631e2e6429dec9623c67e049ea4aeb21ea09ed
Justin Locsei <justin.locsei@gmail.com>
2026-09-09 14:42:20 -0400

    Expand code for managing prop tests

--- src/tests/properties/config.ts
+++ src/tests/properties/config.ts
@@ -1,9 +1,15 @@
+import type { EnvironmentVariables } from '../../types.ts';
+
+// A mapping of property IDs to environment variables
+const VARIABLES = {
+  runs: 'LARKSPUR_PROP_TEST_RUNS',
+  seed: 'LARKSPUR_PROP_TEST_SEED'
+};
+
 /**
  * An environment variable used to configure property tests
  */
-export type ConfigVariable =
-  | 'LARKSPUR_PROP_TEST_RUNS'
-  | 'LARKSPUR_PROP_TEST_SEED';
+export type ConfigVariable = keyof typeof VARIABLES;
 
 /**
  * Get a configuration variable's value
@@ -11,5 +17,23 @@ export type ConfigVariable =
 export function getConfigVariable(
   variable: ConfigVariable
 ): string | undefined {
-  return process.env[variable];
+  return process.env[VARIABLES[variable]];
+}
+
+/**
+ * Set configuration variables
+ */
+export function setConfigVariables(
+  values: Partial<Record<ConfigVariable, number | string>>
+): EnvironmentVariables {
+  return Object.entries(values).reduce<EnvironmentVariables>(
+    (previous, [id, value]) => {
+      if (value !== undefined) {
+        previous[VARIABLES[id as ConfigVariable]] = String(value);
+      }
+
+      return previous;
+    },
+    {}
+  );
 }
--- test/setup/properties.ts
+++ test/setup/properties.ts
@@ -2,10 +2,10 @@ import { fc } from '@fast-check/vitest';
 
 import { getConfigVariable } from '../../src/tests/properties/config.ts';
 
-const testRuns = getConfigVariable('LARKSPUR_PROP_TEST_RUNS');
-const testSeed = getConfigVariable('LARKSPUR_PROP_TEST_SEED');
+const runs = getConfigVariable('runs');
+const seed = getConfigVariable('seed');
 
 fc.configureGlobal({
-  numRuns: testRuns ? Number(testRuns) : 100,
-  seed: testSeed ? Number(testSeed) : undefined
+  numRuns: runs ? Number(runs) : 100,
+  seed: seed ? Number(seed) : undefined
 });
