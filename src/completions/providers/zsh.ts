dcfb1ab6d892f77ccf97b6ace48a97264dc2ce2e
Justin Locsei <justin.locsei@gmail.com>
2026-09-09 18:02:19 -0400

    Use conditional shared flags in completions

--- src/completions/providers/zsh.ts
+++ src/completions/providers/zsh.ts
@@ -238,7 +238,7 @@ Then add this line before loading compinit:
     levels: string[]
   ): ScriptLines {
     const flags: Flags = {
-      ...useSharedFlags(this.config),
+      ...useSharedFlags(this.config, 'command'),
       ...(command.flags || {})
     };
 
