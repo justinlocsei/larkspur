a01dfd2c08e525258849989ff83074356cad6a94
Justin Locsei <justin.locsei@gmail.com>
2026-09-09 12:18:59 -0400

    Switch test filtering

--- tasks/test.ts
+++ tasks/test.ts
@@ -4,16 +4,19 @@ import { run } from './helpers.ts';
 const SUITES = ['integration', 'properties', 'unit'] as const;
 
 export default C({
-  allowExtraArgs: true,
   description: 'Run tests',
   flags: {
+    name: C.flag(
+      'string',
+      'Only run tests in files matching the given pattern'
+    ),
     suite: C.flag('choice', 'Only run the given test suites', {
       choices: SUITES,
       default: SUITES,
       repeatable: true
     })
   },
-  handler: async ({ suite: suites }, { args }) => {
+  handler: async ({ name = '', suite: suites }) => {
     for (const suite of suites) {
       run(
         'vitest',
@@ -22,7 +25,7 @@ export default C({
         suite,
         '--reporter',
         'verbose',
-        ...args.extra
+        name
       );
     }
   }
