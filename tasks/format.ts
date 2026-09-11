261031d09d47574e3cb85cce3336815e4c3e0147
Justin Locsei <justin.locsei@gmail.com>
2026-09-11 07:23:50 -0400

    Use a nested formatting task

--- tasks/format.ts
+++ tasks/format.ts
@@ -1,7 +1,9 @@
 import C from '../src/factory.ts';
 import { run } from './helpers.ts';
 
-export default C('Format the codebase', async () => {
-  run('biome', ['check', '--write', '--linter-enabled=false', '.']);
-  run('dprint', ['fmt']);
+export default C.group('Manage formatting', {
+  code: C('Format the codebase', async () => {
+    run('biome', ['check', '--write', '--linter-enabled=false', '.']);
+    run('dprint', ['fmt']);
+  })
 });
