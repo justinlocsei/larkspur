8d98bc9436f0e9d694d3f5e439d3d361b2e22ad9
Justin Locsei <justin.locsei@gmail.com>
2026-09-13 14:50:47 -0400

    Refine the public types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import factory from './factory.ts';
 
-export type { UserOptions } from './cli.ts';
 export { run } from './cli.ts';
+export type {
+  Command,
+  CommandGroup,
+  CommandHandler,
+  CommandTree
+} from './commands/types.ts';
 export { OperationalError } from './errors.ts';
+export type { Flag, Flags } from './flags/types.ts';
+export type { ValuesOf } from './flags/values.ts';
 
 export default factory;
