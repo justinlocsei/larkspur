05e99cb58d826940237dc3bef6359e6652ab6a8c
Justin Locsei <justin.locsei@gmail.com>
2026-09-14 13:42:42 -0400

    Exclude all test helpers from coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,11 @@ export default defineConfig({
   test: {
     chaiConfig: { truncateThreshold: 0 },
     coverage: {
-      exclude: ['src/**/*.test.ts', 'src/tests/'],
+      exclude: [
+        'src/**/*.test.ts',
+        'src/**/tests.ts',
+        'src/tests/'
+      ],
       include: ['src/**/*.ts'],
       provider: 'v8',
       reporter: ['html', 'text'],
