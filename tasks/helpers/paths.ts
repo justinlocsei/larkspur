fb1dbaf18884ccb0147a3457fe42ea9828bfc652
Justin Locsei <justin.locsei@gmail.com>
2026-09-15 19:01:17 -0400

    Switch command execution

--- tasks/helpers/paths.ts
+++ tasks/helpers/paths.ts
@@ -7,18 +7,6 @@ export const REPO_ROOT = path.resolve(
 );
 
 /**
- * Produce the path to a node_modules executable
- */
-export function localBin(name: string): string {
-  return path.join(
-    REPO_ROOT,
-    'node_modules',
-    '.bin',
-    `${name}${process.platform === 'win32' ? '.cmd' : ''}`
-  );
-}
-
-/**
  * Produce the path to a file relative to the repository root
  */
 export function localFile(name: string): string {
