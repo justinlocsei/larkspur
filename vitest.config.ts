8ae165bb3bb30bace3d3dc34a33c77748744971a
Justin Locsei <justin.locsei@gmail.com>
2026-09-19 07:23:07 -0400

    Rely on default project extension

--- vitest.config.ts
+++ vitest.config.ts
@@ -23,7 +23,6 @@ export default defineConfig({
     environment: 'node',
     projects: [
       {
-        extends: true,
         test: {
           exclude: ['src/**/*.prop.test.ts'],
           include: ['src/**/*.test.ts'],
@@ -31,7 +30,6 @@ export default defineConfig({
         }
       },
       {
-        extends: true,
         test: {
           include: ['test/clis/**/*.test.ts'],
           name: 'integration',
@@ -39,7 +37,6 @@ export default defineConfig({
         }
       },
       {
-        extends: true,
         test: {
           include: ['src/**/*.prop.test.ts'],
           name: 'properties',
